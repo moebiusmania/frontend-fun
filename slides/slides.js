aeecf57cd44547fa17a0c7a2ca98f70bb9113e16
Salvatore Laisa <moebiusmania@gmail.com>
2018-01-30 12:33:14 +0100

    more slides

--- slides/slides.js
+++ slides/slides.js
@@ -3,3 +3,10 @@
 // Reveal + code highlighting
 Reveal.initialize();
 hljs.initHighlightingOnLoad();
+
+
+// Listening to custom events from gamepad-api element
+const gamepad = document.querySelector('gamepad-api');
+gamepad.addEventListener('button', evt => {
+  evt.detail.button === 'left' ? Reveal.prev() : Reveal.next();
+});
